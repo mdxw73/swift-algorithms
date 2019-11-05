81300438371df7467ac2fac17e3aa21e3a63d6d4
Zack Obied <obied.z@etoncollege.org.uk>
2019-11-05 11:11:00 +0000

    Initial check-in of code

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Obied, Zack (NA) on 05/11/2019.
+//  Copyright © 2019 Obied, Zack (NA). All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let name = "Zack Obied"
+
+print(name)
+
+//this is a comment
